dc9a14975078643e00a8b78ee41487a6bbb6ed3a
jchand <jchand@usc.edu>
2018-02-07 22:34:23 -0800

    Navigation.

--- script.js
+++ script.js
@@ -13,3 +13,26 @@ function toggle() {
         document.getElementById('left').style.width = '60%';
     }    
 }
+
+window.smoothScroll = function(target) {
+    var scrollContainer = target;
+    do { 
+        scrollContainer = scrollContainer.parentNode;
+        if (!scrollContainer) return;
+        scrollContainer.scrollTop += 1;
+    } while (scrollContainer.scrollTop == 0);
+
+    var targetY = -100;
+    do { 
+        if (target == scrollContainer) break;
+        targetY += target.offsetTop;
+    } while (target = target.offsetParent);
+
+    scroll = function(c, a, b, i) {
+        i++; if (i > 10) return;
+        c.scrollTop = a + (b - a) / 10 * i;
+        setTimeout(function(){ scroll(c, a, b, i); }, 20);
+    }
+ 
+    scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
+}

a5b5f818524b9c9c69b0a3e82761608d448ef273
jchand <jchand@usc.edu>
2018-02-08 17:25:17 -0800

    Finished content.

--- script.js
+++ script.js
@@ -14,6 +14,83 @@ function toggle() {
     }    
 }
 
+var tab1 = $("#tab1")
+var tab2 = $("#tab2")
+var tab3 = $("#tab3")
+var tab4 = $("#tab4")
+
+var section1 = $("#section1")
+var section2 = $("#section2")
+var section3 = $("#section3")
+var section4 = $("#section4")
+
+var video1 = $("#video1")
+var video2 = $("#video2")
+var video3 = $("#video3")
+
+var currentTab = 0;
+
+setTabColor();
+$("#page").scroll(function() {
+    setTabColor();
+});
+
+function setTabColor() {
+    if (section4.position().top < 150) {
+        if (currentTab != 4) {
+            currentTab = 4;
+            tab4.css("color", "#373C43");
+            tab3.css("color", "#373C4366");
+            tab2.css("color", "#373C4366");
+            tab1.css("color", "#373C4366");
+
+            video3.show();
+            video2.hide();
+            video1.hide();
+            document.getElementById('phone').style.display = 'block';
+        }
+    } else if (section3.position().top < 150) {
+        if (currentTab != 3) {
+            currentTab = 3;
+            tab3.css("color", "#373C43");
+            tab4.css("color", "#373C4366");
+            tab2.css("color", "#373C4366");
+            tab1.css("color", "#373C4366");
+
+            video3.hide();
+            video2.show();
+            video1.hide();
+            document.getElementById('phone').style.display = 'block';
+        }
+    } else if (section2.position().top < 150) {
+        if (currentTab != 2) {
+            currentTab = 2;
+            tab2.css("color", "#373C43");
+            tab4.css("color", "#373C4366");
+            tab3.css("color", "#373C4366");
+            tab1.css("color", "#373C4366");
+
+            video3.hide();
+            video2.hide();
+            video1.show();
+            document.getElementById('phone').style.display = 'block';
+        }
+    } else if (section1.position().top < 150) {
+        if (currentTab != 1) {
+            currentTab = 1;
+            tab1.css("color", "#373C43");
+            tab4.css("color", "#373C4366");
+            tab3.css("color", "#373C4366");
+            tab2.css("color", "#373C4366");
+
+            video3.hide();
+            video2.hide();
+            video1.hide();
+            document.getElementById('phone').style.display = 'none';
+        }
+    }
+}
+
 window.smoothScroll = function(target) {
     var scrollContainer = target;
 
@@ -36,4 +113,15 @@ window.smoothScroll = function(target) {
     }
  
     scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
+}
+
+function seekToTime(video, ts) {
+    video.pause();
+    video.currentTime = ts; 
+    var timer = setInterval(function() {
+        if (video.paused && video.readyState == 4 || !video.paused) {
+            video.play();
+            clearInterval(timer);
+        }
+    }, 50);
 }
